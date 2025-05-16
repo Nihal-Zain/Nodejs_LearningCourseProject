1f0256429afce3769923b948cf0a148859835368
“[Noor <nourmamoun@gmail.com>
2025-05-16 20:28:10 +0300

    Adding subcategory endpoint

--- controllers/course/courseController.js
+++ controllers/course/courseController.js
@@ -9,3 +9,13 @@ exports.getcourses = async (req, res) => {
     res.status(500).json({ error: 'Database error' });
   }
 };
+
+exports.getSubCategories = async(req,res) =>{
+  try {
+    const [results] = await db.promise().query('SELECT DISTINCT(sub_category) FROM `course` WHERE category = ?', [req.params.category]);
+    res.json(results);
+  } catch (err) {
+    console.error('DB Query Error:', err);
+    res.status(500).json({ error: 'Database error' });
+  }
+}
--- routes/course.js
+++ routes/course.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const router = express.Router();
-const { getcourses } = require('../controllers/course/courseController');
+const { getcourses, getSubCategories } = require('../controllers/course/courseController');
 
 router.get('/courses', getcourses);
+router.get('/courses/:category', getSubCategories);
 
 module.exports = router;
