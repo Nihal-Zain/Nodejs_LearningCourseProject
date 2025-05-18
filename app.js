b224bc005b2fb6f6e7cb273d4a49bc3de442b4fa
Nihal Zain <nihalzain30@gmail.com>
2025-05-18 23:56:27 +0300

    change port

--- app.js
+++ app.js
@@ -22,6 +22,10 @@ const app = express();
 app.use(cors({
   origin: 'http://localhost:5173',  
 }));
+app.use(cors({
+  origin: 'http://localhost:3000',  
+}));
+
 app.use(express.json());
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 // Routes
