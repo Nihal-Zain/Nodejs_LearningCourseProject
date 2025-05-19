343acf9f164ec6d91a41ce07d295ba73bbea1cdd
“[Noor <nourmamoun@gmail.com>
2025-05-19 13:52:37 +0300

    Stop tracking .env file

--- app.js
+++ app.js
@@ -19,7 +19,10 @@ const app = express();
 
 // Middleware
 app.use(cors({
-  origin: 'http://localhost:5173',  
+  origin: [
+    'http://localhost:5173',
+    'http://localhost:3000',
+  ]
 }));
 app.use(express.json());
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
