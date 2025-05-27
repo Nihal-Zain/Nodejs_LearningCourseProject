dbb0b395cde8021d07628deb14ef77cb0769ad9c
Nihal Zain <nihalzain30@gmail.com>
2025-05-28 00:34:47 +0300

    Add SubSection

--- controllers/sectionController/sectionController.js
+++ controllers/sectionController/sectionController.js
@@ -2,15 +2,16 @@ const e = require('express');
 const db = require('../../config/db');
 const path = require('path');
 
+// Get all sections
 exports.getAllSections = (req, res) => {
     const query = 'SELECT * FROM sections ORDER BY id DESC';
     db.query(query, (err, results) => {
         if (err) return res.status(500).json({ error: err.message });
         res.json(results);
-    }
-    );
+    });
 }
 
+// Get section by ID
 exports.getSectionById = (req, res) => {
     const sectionId = req.params.id;
     const query = 'SELECT * FROM sections WHERE id = ?';
@@ -21,29 +22,30 @@ exports.getSectionById = (req, res) => {
     });
 };
 
+// Create a new section
 exports.createSection = (req, res) => {
-    const { title} = req.body;
-
-    const query = 'INSERT INTO sections (title) VALUES (?)';
-    db.query(query, [title], (err, result) => {
+    const { title, ismanager = 0 } = req.body;
+    const query = 'INSERT INTO sections (title, ismanager) VALUES (?, ?)';
+    db.query(query, [title, ismanager], (err, result) => {
         if (err) return res.status(500).json({ error: err.message });
-
-        res.status(201).json({ id: result.insertId, title });
+        res.status(201).json({ id: result.insertId, title, ismanager });
     });
 };
 
+// Update a section
 exports.updateSection = (req, res) => {
     const sectionId = req.params.id;
-    const { title } = req.body;
+    const { title, ismanager } = req.body;
 
-    const query = 'UPDATE sections SET title=? WHERE id = ?';
-    db.query(query, [title, sectionId], (err, result) => {
+    const query = 'UPDATE sections SET title = ?, ismanager = ? WHERE id = ?';
+    db.query(query, [title, ismanager, sectionId], (err, result) => {
         if (err) return res.status(500).json({ error: err.message });
         if (result.affectedRows === 0) return res.status(404).json({ error: 'Section not found' });
         res.json({ message: 'Section updated successfully' });
     });
 };
 
+// Delete a section
 exports.deleteSection = (req, res) => {
     const sectionId = req.params.id;
     const query = 'DELETE FROM sections WHERE id = ?';
