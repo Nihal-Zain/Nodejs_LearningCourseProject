abdec8b1809a1d62fa33008933298a71f52952f6
“[Noor <nourmamoun@gmail.com>
2025-06-01 19:36:13 +0300

    Refactor code structure for improved readability and maintainability

--- routes/instructors.js
+++ routes/instructors.js
@@ -1,15 +1,28 @@
+// routes/instructorRoutes.js
 const express = require('express');
 const router = express.Router();
+const path = require('path');
 const multer = require('multer');
-const { storage } = require('../middlewares/cloudinary'); // adjust path
-const upload = multer({ storage });
-
 const instructorController = require('../controllers/instructors/instructorsController');
 
+// Multer configuration
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, 'uploads/');
+  },
+  filename: (req, file, cb) => {
+    const uniqueName = Date.now() + path.extname(file.originalname);
+    cb(null, uniqueName);
+  }
+});
+
+const upload = multer({ storage });
+
+// Routes
 router.get('/instructors', instructorController.getAllInstructors);
-router.post('/instructors', upload.single('img'), instructorController.addInstructor);
 router.get('/instructors/:id', instructorController.getInstructorById);
-router.delete('/instructors/:id', instructorController.deleteInstructor);
+router.post('/instructors', upload.single('img'), instructorController.addInstructor);
 router.put('/instructors/:id', upload.single('img'), instructorController.updateInstructor);
+router.delete('/instructors/:id', instructorController.deleteInstructor);
 
 module.exports = router;
